fbb0d208b320c60454033df35eea885427b78dc9
jandrey <jandrey.serna@utp.edu.co>
2021-03-30 17:41:07 -0500

    separe el index

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset=utf-8>
+  <title>Emmaus</title>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php wp_head(); ?>
+</head>
+<body>
+
+  <img src="wp-content/themes/emmauspag/img/LOGOSOLO.png" class="logo">
--- index.php
+++ index.php
@@ -1,68 +1,54 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset=utf-8>
-  <title>Emmaus</title>
 
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<?php wp_head(); ?>
-</head>
-<body>
 
-  <meta http-equiv="Expires" content="0">
-  <meta http-equiv="Last-Modified" content="0">
-  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
-  <meta http-equiv="Pragma" content="no-cache">
 
-  <img src="wp-content/themes/emmauspag/img/LOGOSOLO.png" class="logo">
+
   <?php
-  require_once "modelo.php";
-  require_once 'envio.php';
-  require_once 'bakend.php';
+  get_header();
+  $my_post = get_post();
+
+  #print_r($my_post);
+
+#  if ($my_post->ID = 55){
+   echo $my_post->post_content ;
+#  }
 
 
+#  require_once "modelo.php";
+#  require_once 'envio.php';
+#  require_once 'bakend.php';
 
-    $url =trim($_SERVER['REQUEST_URI'],'/') ;
-    $url = explode('/',$url);
+    #$url =trim($_SERVER['REQUEST_URI'],'/') ;
+    #$url = explode('/',$url);
 
-    if ( array_search('estudiantes', $url) )
-    {
-      require_once "vistas/visEstudiante.php";
-    }  else if (array_search('usuarios', $url))
-    {
-      require_once "vistas/usuarios.php";
-    }else if (array_search('cursos', $url))
-    {
-      require_once "vistas/cursos.php";
-    }else if (array_search('factura', $url))
-    {
-      require_once "vistas/facturas.php";
-    }else if (array_search('caja', $url))
-    {
-      require_once "vistas/caja.php";
-    }else if (array_search('inventario', $url))
-    {
-      require_once "vistas/inventario.php";
-    }else if (array_search('imprimir', $url))
-    {
-      require_once "vistas/imprimir.php";
-    }else if (array_search('certificado', $url))
-    {
-      require_once "vistas/certificado.php";
-    }else
-    {
-      require_once "vistas/principal.php";
-    }
+    #if ( array_search('principal', $url) )
+    #{
+    #}
+
+    #}  else if (array_search('usuarios', $url))
+    #{
+    #  require_once "vistas/usuarios.php";
+    #}else if (array_search('cursos', $url))
+    #{
+    #  require_once "vistas/cursos.php";
+    #}else if (array_search('factura', $url))
+    #{
+    #  require_once "vistas/facturas.php";
+    #}else if (array_search('caja', $url))
+    #{
+    #  require_once "vistas/caja.php";
+    #}else if (array_search('inventario', $url))
+    #{
+    #  require_once "vistas/inventario.php";
+    #}else if (array_search('imprimir', $url))
+    #{
+    #  require_once "vistas/imprimir.php";
+    #}else if (array_search('certificado', $url))
+    #{
+    #  require_once "vistas/certificado.php";
+    #}else
+    #{
+    #  require_once "vistas/principal.php";
+    #}
 
 
   ?>
-
-
-  </div>
-
-</div>
-</body>
-</html>
